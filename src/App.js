92dc1471f80a570037b2c35e4b33f59e0e4948b9
Peter Okorafor <peterdtitan@gmail.com>
2023-04-19 13:23:22 +0300

    Add template for missions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <Missions />
+      </div>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import logo from '../assets/images/planet.png';
 
 export default function Footer() {
   return (
-    <div className="w-full absolute bottom-0 flex bg-black/80 justify-center items-center p-2 text-sm text-white">
+    <footer className="flex flex-col text-white text-xs md:text-base md:flex-row items-center justify-center bg-slate-900 gap-10 fixed bottom-0 w-screen h-10 font-montserrat">
       <div className="flex gap-2 items-center">
         <img src={logo} alt="logo" className="w-8 invert" />
         <p className="text-xs md:text-base">Created by Microverse Inc under license</p>
       </div>
-    </div>
+    </footer>
   );
 }
