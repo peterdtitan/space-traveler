c1de1d537089f9a0a8db3ef5ff6666dd77567a5e
Peter Okorafor <peterdtitan@gmail.com>
2023-04-22 09:15:35 +0300

    Update Profile Page with 'filter'

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,22 +5,29 @@ export default function Profile() {
   const { missions } = useSelector((state) => state.missions);
   const { rockets } = useSelector((state) => state.rockets);
 
+  const joinedMissions = missions.filter((mission) => mission.isJoined);
+  const reservedRockets = rockets.filter((rocket) => rocket.isBooked);
+
   return (
     <div className="profileContainer">
       <div className="missionContainer">
         <h2>My Missions</h2>
         <div className="pmData">
-          {missions.map((mission) => (
-            <p key={mission.id}>{mission.name}</p>
-          ))}
+          {joinedMissions.length > 0 ? (
+            joinedMissions.map((mission) => <p key={mission.id}>{mission.name}</p>)
+          ) : (
+            <p>No Missions Joined</p>
+          )}
         </div>
       </div>
       <div className="rocketContainer">
         <h2>My Rockets</h2>
         <div className="prData">
-          {rockets.map((rocket) => (
-            <p key={rocket.id}>{rocket.rocketName}</p>
-          ))}
+          {reservedRockets.length > 0 ? (
+            reservedRockets.map((rocket) => <p key={rocket.id}>{rocket.name}</p>)
+          ) : (
+            <p>No Rockets Reserved</p>
+          )}
         </div>
         <br />
       </div>
